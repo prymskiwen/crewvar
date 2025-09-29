1808ad8f306af1967d0401db7f4495069e722a91
prymskiwen <prymskiwen.dev@gmail.com>
2025-09-28 20:20:54 -0700

    update: logout button on admin AppBar

--- src/components/admin/layout/AdminAppBar.tsx
+++ src/components/admin/layout/AdminAppBar.tsx
@@ -48,7 +48,7 @@ export const AdminAppBar: React.FC<AdminAppBarProps> = ({
                             <svg className="h-4 w-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                             </svg>
-                            <span className="hidden sm:inline">Logout</span>
+                            {/* <span className="hidden sm:inline">Logout</span> */}
                         </button>
                     </div>
                 </div>
